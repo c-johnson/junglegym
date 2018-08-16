86e92ae46dba928730f537835acc8dfd43fdc2c1
Chris Johnson <chris@cjohnson.io>
2018-08-16 16:05:18 -0700

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <p style={{margin: '40px auto', width: '960px'}}>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    );
+  }
+}
+
+export default App;
